23529aabc57a7a2bb31c7f877843c0eb21a81284
thgcamposantos <thgcamposantos@gmail.com>
2024-06-28 21:11:22 -0300

    Final do dia 28/06/2024

---
+++ secao2/aula3.py
@@ -0,0 +1,11 @@
+# age = "24"
+# name = str('thiago')
+# print(f'Meu nome é: {name} e tenho {age} anos')
+
+# print(type(name))
+
+# age = int(age)
+
+# print(type(age))
+nome = input('Seu nome: ')
+print(nome)
