e03d63b570eff2f248da9d92e4c321c281a6478d
thgcamposantos <thgcamposantos@gmail.com>
2024-06-29 11:46:46 -0300

    Fim do 3 módulo

---
+++ secao3/aula1_3.py
@@ -0,0 +1,18 @@
+# numero1 = int(input('Digite um número: '))
+# numero2 = int(input('Digite um outro número: '))
+
+# print('Adição é: ' , numero1 + numero2)
+
+# subtração (-) 
+# multiplicação (*) 
+# divisão (/) 
+# divisão inteira (//) 
+# módulo (%)) 
+# exponenciação (**) 
+
+# - Aula 2
+
+saldo = int(input('Qual seu saldo: '))
+saque = int(input('Quanto deseja retirar: '))
+print(saldo > saque)
+saldo_final = print('Saldo final é: ' , (saldo - saque))
